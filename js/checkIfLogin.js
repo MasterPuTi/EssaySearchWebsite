386b83005196dffbe677806bacc3c4df3bebb286
LZR <1581501440@qq.com>
2018-06-13 17:03:29 +0800

    修改密码和注销

--- js/checkIfLogin.js
+++ js/checkIfLogin.js
@@ -1,10 +1,27 @@
-jQuery(document).ready(function( $ ) {
-
-
-
-});
-
-function checkALL() {
+function logout(){
+    $.ajax({
+        contentType: 'application/json;charset=UTF-8',
+        url:'http://192.144.179.57:8080/demo-v1/api/authen/logout',
+        type:'delete',
+        dataType: "json",
+        success: function(data){
+            if (data) {
+                console.log(data);
+                if(data.status === "succeed"){
+                    location.reload();
+                }
+            }else{
+                alert('net failure');
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            // alert(XMLHttpRequest.status);
+            // alert(XMLHttpRequest.readyState);
+            // alert(textStatus);
+        }
+    });
+}
+function checkLogin() {
     $.ajax({
         contentType: 'application/json;charset=UTF-8',
         url:'http://192.144.179.57:8080/demo-v1/api/authen/user',
@@ -12,7 +29,6 @@ function checkALL() {
         dataType: "json",
         success: function(data){
             if (data) {
-                //alert("success");
                 console.log(data);
                 if(data.status === "succeed"){
                     var loginButton = document.getElementById("loginButton");
@@ -21,9 +37,9 @@ function checkALL() {
                         '欢迎，' + data.nickname;
                     registerButton.innerHTML=
                         '<a href="user.html?nickname=' + data.nickname + '">'+ '个人空间' + '</a>';
+                    document.getElementById("changePassword").style.display = "visible";
+                    document.getElementById("logout").style.display = "visible";
                 }
-                else
-                    //alert(data.info);
             }else{
                 alert('net failure');
             }
@@ -36,4 +52,4 @@ function checkALL() {
     });
 }
 
-window.onload=checkALL;
+window.onload=checkLogin;
