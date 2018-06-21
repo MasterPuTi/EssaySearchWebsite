20892da03cbe503ad244ef0fa2eaadbb4e64ca28
LegendFC <449106803@qq.com>
2018-06-22 00:04:53 +0800

    学科展示页面

--- js/resultpage.js
+++ js/resultpage.js
@@ -27,7 +27,9 @@ function resultInfo() {
                     var result_name=document.getElementById("result_name");
                     var result_ownersName=document.getElementById("result_ownersName");
                     var result_publicationTime=document.getElementById("result_publicationTime");
+                    var result_referenceTimes=document.getElementById("result_referenceTimes");
                     var result_abstractInfo=document.getElementById("result_abstractInfo");
+                    var result_purchase=document.getElementById("result_ purchase");
                     //如果该论文已收藏 显示实心star
                     if (data.data.collected){
                         result_name.innerHTML=
@@ -42,21 +44,37 @@ function resultInfo() {
                         '                                <a id="del-collect-btn" style="display: none">'+'<i class="fa fa-star"'+' onclick="delFromCollection('+data.data.id+')"></i></a>';
                     }
 
+                    result_purchase.innerHTML=
+                        '<span class="address-title" onclick="purchase('+data.data.id+')">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Purchase</span>';
+
+                    /*for(var j=0;j<data.data.ownersName.length;j++) {
+                        if (j){
+                            result_ownersName.innerHTML += '，';
+                        }
+                        result_ownersName.innerHTML+='<a href="professor.html?pid='+data.data.ownersName[j].name;
+                    }*/
+                    //显示作者
                     for(var j=0;j<data.data.ownersName.length;j++) {
-                            result_ownersName.innerHTML+=data.data.ownersName[j].name;
+                        if (j){
+                            result_ownersName.innerHTML += '，';
+                        }
+                        result_ownersName.innerHTML += '<a href="professor.html?pid=' + data.data.ownersName[j].id + '">' + data.data.ownersName[j].name + '</a>';
                     }
                     //result_ownersName.innerHTML=
                         //data.data.ownersName.id;
                     result_publicationTime.innerHTML=
                         data.data.publicationTime;
-            result_abstractInfo.innerHTML=
-                data.data.abstractInfo;
-        }
-        else
-            alert(data.status);
-}else{
-        alert('net failure');
-    }
+                    result_referenceTimes.innerHTML=
+                        data.data.referenceTimes;
+                    result_abstractInfo.innerHTML=
+                        data.data.abstractInfo;
+                }
+                else
+                    alert(data.status);
+            }
+            else{
+                    alert('net failure');
+            }
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             alert(XMLHttpRequest.status);
@@ -65,6 +83,34 @@ function resultInfo() {
         }
     });
     return false;
+}
+
+function purchase(paperId){
+    $.ajax({
+        contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
+        url:'http://192.144.179.57:8080/demo-v1/api/user/paper/'+paperId+'/buy',
+        type:'get',
+        dataType: "json",
+        success: function(data){
+            if (data) {
+                if (res.status==='succeed'){           //success
+                    alert('购买成功');
+                }
+                else {
+                    alert(data.status);
+                }
+            }else{
+                alert('net failure');
+                //没有登录就跳转到index.html
+                window.location.href = 'index.html';
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            // alert(XMLHttpRequest.status);
+            // alert(XMLHttpRequest.readyState);
+            // alert(textStatus);
+        }
+    });
 }
 
 function addToCollection(paperId){
